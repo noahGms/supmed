f8020a3a20dc5a0eee09ea89e39e1e381aece99a
noahGms <noah@keshin.net>
2021-06-20 15:27:56 +0200

    added fontawesome link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/4f3834f0ec.js" crossorigin="anonymous"></script>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <style>
             body {
